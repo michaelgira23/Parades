8d5ca6608245cdfe14c7c1a0331bcecc180e1327
Michael <michaelgira23@gmail.com>
2016-03-17 13:49:25 -0500

    Setup Express Server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var port = 666;
+
+var http    = require('http');
+var express = require('express');
+var app     = express();
+var server  = http.Server(app);
+var io      = require('socket.io');
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/html/index.html');
+});
+
+server.listen(port, function() {
+	console.log('Server listening on *:' + port);
+});
